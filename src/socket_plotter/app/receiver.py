26e9737e108517e47bdb04c0a00e0151086113d8
Kosuke Mizuno <43668684+KosukeMizuno@users.noreply.github.com>
2022-08-14 04:13:39 +0900

    add todo comment

--- src/socket_plotter/app/receiver.py
+++ src/socket_plotter/app/receiver.py
@@ -12,6 +12,7 @@ class QThreadReceiver(QtCore.QThread):
     1. 接続を受けたら、以下のようなdict (header) を受け取る
         {'size': int, 'type': Literal['data', 'attr', 'ping']}
         - type==ping なら何もせずに次の接続をまつ
+        - TODO: headerはjsonで受けて、データ自体もjsonで受けるモードを作りたい。現状だとpython限定だしバージョン合わせの問題が出るかも
     2. `A header was received.` と返す
     3. (1)で受けたサイズだけデータを受け取る
     4. type==data ならsigDataでui側へ渡す。type==attrならsigAttrでui側へ渡す
